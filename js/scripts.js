94172305e7794a74cc99e70a9fca782c33c07481
jusuzuki <jsayumi@gmail.com>
2015-09-27 16:49:05 -0700

    Add initial functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,34 @@
-var bottlesOfBeer = function(number) {
-  
-  if (number === 0) {
-    return ' No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.';
-  } else if (number === 1) {
-    return ' 1 bottle of beer on the wall, 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.' +  bottlesOfBeer(number - 1);
-  } else if (number === 2) {
-    return ' 2 bottles of beer on the wall, 2 bottles of beer.  Take one down, pass it around, 1 bottle of beer on the wall.' +  bottlesOfBeer(number - 1);
-  } else {
-    return number + ' bottles of beer on the wall, ' + number + ' bottles of beer. Take one down and pass it around, ' + (number - 1) + ' bottles of beer on the wall.' +  bottlesOfBeer(number - 1);
-    }
-  }
+var Pizza = function(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
 
+Pizza.prototype.getPrice = function() {
+  var price = this.size + (this.toppings * 0.50);
+  return price;
+}
 
 $(document).ready(function() {
-  $("form#bottles_of_beer").submit(function(event) {
-    var number = parseInt($("input#number").val());
-    if(isNaN(number)) {
-      $(".song").text("Can't sing to that!");
-    } else if (number < 0) {
-      $(".song").text("Sorry, too drunk to sing negative numbers!");
-    } else {
-      var result = bottlesOfBeer(number);
-      $(".song").text(result);
-    }
+  $("form#pizza").submit(function(event) {
+
+    var toppings = 0;
+    var toppingsArray = [];
+      $(".toppings:checked").each(function() {
+        toppingsArray[toppings++] = parseInt($(this).val());
+      });
+
+    var size = parseInt($('input[name="size"]:checked').val());
+    var quantity = parseInt($("input#pizza_quantity").val());
+
+    $(".toppings").text(toppings);
+    $(".size").text(size);
+    $(".quantity").text(quantity);
+
+    var order = new Pizza(toppings, size);
+    var total = order.getPrice() * quantity;
+    $(".total").text(total);
 
     $("#result").show();
     event.preventDefault();
-  })
-})
+  });
+});
